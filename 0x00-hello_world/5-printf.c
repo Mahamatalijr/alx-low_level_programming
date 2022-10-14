654a9108370d5c17a58840e88ebaaf9a51238e62
Mahamat Ali Brahim Khalil <mahamatalibrahimkhalil4@gmail.com>
2022-10-14 09:35:18 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+# include <stdio.h>
 /**
- * main - Entry point 
- *My hello, print task
+ * main - Entry point
+ * My hello, print task
  * Return: return 1 after program execution
  */
 int main(void)
-{	
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+		return (0);
 }
