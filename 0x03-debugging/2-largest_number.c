2dcf1789ae78dd03193a39a3edf48e5ad3d3d9ab
Mahamat Ali Brahim Khalil <mahamatalibrahimkhalil4@gmail.com>
2022-10-22 12:56:02 -0700

    the largest of three integers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,23 +9,36 @@
 */
 
 
-
 int largest_number(int a, int b, int c)
+
 {
-	  int largest;
 
-	  if (a >= b && a >= c)
-	  {
-		  largest = a;
-	  }
-	  else if (b >= a && b >= c)
-	  {
-		  largest = b;
-	  }
-	  else
-	  {
-		  largest = c;
-	  }
+	int largest;
 
-	  return (largest);
+	if (a >= b && a >= c)
+
+	{
+
+		largest = a;
+
+	}
+
+	else if (b >= a && b >= c)
+
+	{
+
+		largest = b;
+
+	}
+
+	else
+
+	{
+
+		largest = c;
+
+	}
+
+	return (largest);
+
 }
