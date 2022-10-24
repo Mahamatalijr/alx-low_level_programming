b32820cd2d81798e52e2cc6b4f91b407fd1d6868
Mahamat Ali Brahim Khalil <mahamatalibrahimkhalil4@gmail.com>
2022-10-24 06:07:03 -0700

    Winning is only half of it and Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,36 +1,38 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * puts_half - prints a string
- * @str: string to print
- * Return: nothing
+ * puts_half - prints half of a string.
+ * @str: input string.
+ * Return: void.
  */
-
 void puts_half(char *str)
 {
-	int i, len = _strlen(str);
+	int length = 0, n;
 
-	for (i = ((len - 1) / 2) + 1; i < len; i++)
-		putchar(*(str + i));
-	putchar(10);
-}
+	while (length >= 0)
+	{
+		if (str[length] != '\0')
+		{
+			length++;
+		}
+		else
+		{
+			break;
+		}
+	}
 
+	if (length % 2 == 0)
+		n = length / 2;
+	else
+	{
+		n = (length - 1) / 2;
+		n = n + 1;
+	}
 
- /**
-  * _strlen - returns the length of a string
-  * @s: string
-  * Return: the length of the given string
-  */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-
-	while (*(s + len) != '\0')
-		len++;
-
-
-	return (len);
+	while (n < length)
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
 }
